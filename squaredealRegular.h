a60abafbd2d62b776cb8e281128a64e1b03a020f
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:36:18 +0100

    Initial import

---
+++ squaredealRegular.h
@@ -0,0 +1,29 @@
+#ifndef _SQUAREDEALREGULAR_H
+#define _SQUAREDEALREGULAR_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t squaredealRegular10[];
+    extern const uint8_t squaredealRegular11[];
+    extern const uint8_t squaredealRegular12[];
+    extern const uint8_t squaredealRegular13[];
+    extern const uint8_t squaredealRegular14[];
+    extern const uint8_t squaredealRegular15[];
+    extern const uint8_t squaredealRegular16[];
+    extern const uint8_t squaredealRegular17[];
+    extern const uint8_t squaredealRegular18[];
+    extern const uint8_t squaredealRegular19[];
+    extern const uint8_t squaredealRegular20[];
+    extern const uint8_t squaredealRegular21[];
+    extern const uint8_t squaredealRegular22[];
+    extern const uint8_t squaredealRegular23[];
+    extern const uint8_t squaredealRegular24[];
+    extern const uint8_t squaredealRegular25[];
+    extern const uint8_t squaredealRegular26[];
+    extern const uint8_t squaredealRegular27[];
+    extern const uint8_t squaredealRegular28[];
+    extern const uint8_t squaredealRegular29[];
+    extern const uint8_t squaredealRegular30[];
+};
+#endif
